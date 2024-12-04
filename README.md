2d76ada0381b44208be63abe2e8891a132f3e90e
jcsusbilla <jcsusbilla@gmail.com>
2024-12-04 12:33:42 -0800

    API SETUP

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CST205-Final-Project
 Final Project
+
+------------------ Spotify API ------------------ 
+video: https://www.youtube.com/watch?v=WAmEZBEeNmg
+Client ID: ccde7e87d847406385d72de3862c4027
+Client Secret: a49033b7c5ff4ec8951c6a96d60f4519
+
+Terminal Installs:
+- pip install python-dotenv
+- pip install requests
--- interface.py
+++ interface.py
@@ -13,6 +13,54 @@ from PIL import Image
 from PIL.ImageQt import ImageQt
 import os
 
+# --------------------- load in environment variables ---------------------
+# pip install python-dotenv
+# pip install requests
+from dotenv import load_dotenv
+import base64
+import json
+from requests import post, get                                   # allow to send post request
+load_dotenv()
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+#print(client_id, client_secret) # TEST
+
+# retrieve and post access token for Spotify API
+def get_token():
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+def get_auth_header(token):
+    return {"Authorization": "Bearer "+ token}
+
+def search_for_artist(token, artist_name):
+    url = "https://api.spotify.com/v1/search"               # define url
+    headers = get_auth_header(token)                        # get headers
+    # contruct query
+    query = f"?q={artist_name}&type=artist&limit=1"          #return top 1 artist\
+    
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)
+    print(json_result)
+
+token = get_token()
+search_for_artist(token, "Bruno Mars")
+#print("token: " + token) #TEST
+
+# --------------------- GUI INTERFACE ---------------------
+
 class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
@@ -64,9 +112,9 @@ class MyWindow(QWidget):
     #     self.new_win = NewWindow(my_list[i], user_input)                # open window
     #     self.new_win.show()
 
-# call functions
-app = QApplication([])
-win = MyWindow()
-win.show()
-sys.exit(app.exec())
+# --------------------- FUNCTION CALLS ---------------------
+# app = QApplication([])
+# win = MyWindow()
+# win.show()
+# sys.exit(app.exec())
 
